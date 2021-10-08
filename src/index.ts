0197bf4ac5d6e971115cb1845d44d57788cdb891
Johannes Smidelöv <johannes@smidelov.com>
2021-10-08 13:08:10 +0200

    The index.ts is now mostly stitching the other files together

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-interface Video {
-  id: number;
-  title: string;
-  grade: number;
-}
+import { ListElement } from './list-element';
+import {Video} from './model';
+import {VideoApi} from './api';
 
-// GET http://localhost:3000/videos
-const getVideos = (): Promise<Video[]> => {
-}
+let state:Video[] = new Array<Video>();
 
-// PUT http://localhost:3000/videos/${id}
-const updateVideo = async (id, changes): Promise<Video> => {};
+console.log('index.ts is loaded');
+
+const search = ()=>{
+  const searchButton = document.querySelector('#search') as HTMLButtonElement;
+  // Jag låser knappen under pågående hämtning och listgenerering för att throttla anropen,
+  // eftersom inget hindrar användaren att "button mash"a sökningen annars
+  searchButton.disabled = true;
+  let query:string = (document.querySelector('#query') as HTMLInputElement).value;
+  VideoApi.get(query)
+    .then((response:Video[]) => {state = response});
+  generateList();
+  searchButton.disabled = false;
+};
+
+document.querySelector('#search').addEventListener('click', search);
+
+const generateList = ():void => {
+  state.forEach(element => {
+    let results:HTMLElement = document.querySelector('#results');
+    new ListElement(element).render(results, element);
+  });
+}
